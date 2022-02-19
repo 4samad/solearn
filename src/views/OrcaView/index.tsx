14a89100ad9383de909e630a9a1ae75057fa8dae
vcidst <hello@shailendra.me>
2022-02-19 13:35:37 +0400

    add quiz content

--- src/views/OrcaView/index.tsx
+++ src/views/OrcaView/index.tsx
@@ -39,6 +39,48 @@ export const OrcaView: FC = ({}) => {
         </div>
 
         <div className="text-center pt-2">
+        Orca is a Decentralised Exchange (DEX) on Solana.   It allows you to exchange your coins and tokens as well as provide liquidity for exchanges.
+
+Question 1
+Go to https://www.orca.so/ and select “Exchange” from the menu.  Next, connect your wallet to Orca.  Then select from the drop down lists the different tokens.  
+
+Which of the following is a list of suitable assets to swap on Orca? (Available in Drop downs)
+SOL for USDC 
+SOL for SEGWIT BTC
+SOL, FORD F150, 747
+
+Question 2
+The service of providing swaps has many components that will affect the final amount you receive of a desired coin or token.  
+
+What are some of the factors that affect the final outcome you receive? 
+Internet charges
+Liquidity provider fees, Transaction fees & Slippage
+Transaction fees & GAS  
+
+Question 3
+Orca allows you to participate in earning a portion of the Liquidity provider fees.  To participate you will need to supply a trading pair for a return of  % of the fees.
+
+Which of the follow is an example of a trading pair on Orca
+SOL / SOL
+SOL / USDC
+CELO/ADA
+
+Question 4
+Trading pairs display certain percentage of return in APR.  Click on the APR ? extended information to understand how the APR is calculated
+
+Which of the following represent an APR calculation
+Minimum APR returned 
+Estimated APR return from your deposited assets (Based on Last 7 days) 
+100% APR x 1000% APR
+
+Question 5
+Once you provide assets to a Trading Pair, they will no longer appear in your wallet.  Instead you will receive a LP Token that represents your deposited tokens.
+
+Which of the follow is an easy way to find your assets
+Call your Mum
+Select “Pools” and then “Your Liquidity”
+All of the above
+
         </div>
       </div>
     </div>
--- src/views/TulipView/index.tsx
+++ src/views/TulipView/index.tsx
@@ -39,6 +39,44 @@ export const TulipView: FC = ({}) => {
         </div>
 
         <div className="text-center pt-2">
+        TULIP is a Yeild farming aggregator that automates and compounds your earnings.  It is perfectly suited to a fast and low cost blockchain like Solana.
+
+Question 1
+Go to https://tulip.garden/ and connect your wallet.  Launch the app.  Have a look at the menu’s options such and the APY’s being offered
+
+Which of the following best describes some of the options and strategies on TULIP?
+WAGMI
+TULIP offers options such as Lending, Auto Vaults and Leveraged Farming
+My mum loves Tulip Gardens
+
+Question 2
+There are always risks in any protocol, TULIP offers at least 3 different options with different risks vs rewards strategies  
+
+Which of the following best describes the risks on TULIP
+It’s on Solana so the risks are low
+TULIP is a money printing machine with low risks
+TULIP offers lending with lower risk, Auto Vaults with medium risks and Leveraged Farming with high risks
+
+Question 3
+Have a look at the Lending, Auto Vaults and Leveraged Farming options and the different APY they are offering
+
+Which of the follow best describes TULIP’s strategies
+It waters the TULIPS regularly to ensure great growth
+Lending has High APY and Leverage Farming is low risk
+The higher the APY the higher the risks involved.  
+
+Question 4
+Which of the following are possible outcomes on TULIP?
+High risks offer great APY but can result in liquidation 
+High APY = Lambo (Lamborghini money on way) 
+APY > 100% is safe as a bank
+
+Question 5
+What is a good idea when investing in any protocol?
+Work hard as that always makes the most money
+Be smart and invest wisely, don’t risk anything you can’t afford to loose
+TULIP is a great DAPP, i am going all in
+
         </div>
       </div>
     </div>
